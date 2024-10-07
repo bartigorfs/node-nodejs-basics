69e48125599d521bc103125e63b7177867cddd21
Igor Bartashevich <86968311+bartigorfs@users.noreply.github.com>
2024-10-07 21:17:22 +0300

    feat: child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,31 @@
+import {spawn} from "child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+
+    const cp = spawn('node',
+        [import.meta.dirname + '/files/script.js', ...args],
+        {
+            stdio: ['pipe', 'pipe', 'inherit'],
+        });
+
+    cp.stdout.on('data', (data) => {
+        console.log(`Received from child process: ${data}`);
+    });
+
+    cp.stdin.write(args[0]);
+
+    cp.on('message', (message) => {
+        console.log(`Received message from child process: ${message}`);
+    });
+
+    cp.on('close', (code) => {
+        console.log(`Child process exited with code ${code}`);
+    });
+
+    cp.stdin.on('data', (data) => {
+        cp.stdin.write(data);
+    })
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['Shrek is life', 'Shrek is love']);
