f04a1143fae8c6f06f970f75fcc4274cf2ab2fb4
Igor Bartashevich <86968311+bartigorfs@users.noreply.github.com>
2024-10-07 21:55:23 +0300

    feat: zlib compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,28 @@
+import fs from "node:fs/promises";
+import {join} from "path";
+import {promisify} from "node:util";
+import {pipeline} from "node:stream";
+import {createGzip} from "node:zlib";
+import {createReadStream, createWriteStream} from "node:fs";
+
+const FILE_PATH = join(import.meta.dirname, 'files', 'fileToCompress.txt');
+const SAVE_COMPRESSED = join(import.meta.dirname, 'files', 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+    try {
+        await fs.access(FILE_PATH, fs.constants.F_OK).then(async () => {
+            const pipe = promisify(pipeline);
+            const gzip = createGzip();
+
+            const readStream = createReadStream(FILE_PATH);
+            const writeStream = createWriteStream(SAVE_COMPRESSED);
+
+            await pipe(readStream, gzip, writeStream)
+                .then(() => process.stdout.write(`File location: ${SAVE_COMPRESSED}`));
+        })
+    } catch (e) {
+        throw new Error(`Error during compression: ${e.message}`);
+    }
 };
 
 await compress();
