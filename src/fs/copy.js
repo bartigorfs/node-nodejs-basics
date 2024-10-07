def86c65a1742836749307af2a77892abd31250f
Igor Bartashevich <86968311+bartigorfs@users.noreply.github.com>
2024-10-07 22:22:40 +0300

    feat: fs copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,32 @@
+import * as fs from 'node:fs/promises';
+
+import {join} from "path";
+
+const BASE_DIR = join(import.meta.dirname,'files');
+const CP_FOLDER_PATH = join(import.meta.dirname, 'files_copy');
+
 const copy = async () => {
-    // Write your code here 
+    console.log(import.meta.dirname)
+    try {
+        await fs.access(CP_FOLDER_PATH, fs.constants.F_OK);
+        throw new Error('FS operation failed');
+    } catch (e) {
+        if (e.code === 'ENOENT') {
+            try {
+                await fs.access(BASE_DIR);
+
+                await fs.mkdir(CP_FOLDER_PATH);
+
+                await fs.cp(BASE_DIR, CP_FOLDER_PATH, { recursive: true });
+
+                console.log(`Directory copied to path: ${CP_FOLDER_PATH}`);
+            } catch (err) {
+                throw new Error(`Copy operation failed: ${err.message}`);
+            }
+        } else {
+            throw new Error(`Error while copying: ${e.message}`);
+        }
+    }
 };
 
 await copy();
