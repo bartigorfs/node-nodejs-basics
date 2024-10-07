8bf832524dabb24b820c278210ec13867f508862
Igor Bartashevich <86968311+bartigorfs@users.noreply.github.com>
2024-10-07 21:01:32 +0300

    feat: parse command line arguments

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const ARGS = process.argv.slice(2);
+
+    const STDOUT_STR = ARGS.reduce((accum, key, index, arr) => {
+        if (index % 2 === 0) accum.push(`${key} is ${arr[index+1]}`);
+        return accum;
+    }, []).join(', ');
+
+    console.log(STDOUT_STR);
 };
 
 parseArgs();
