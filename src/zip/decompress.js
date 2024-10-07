6a7375a3a67faec3c7f6906ece878107702ec7ee
Igor Bartashevich <86968311+bartigorfs@users.noreply.github.com>
2024-10-07 22:02:15 +0300

    feat: zlib decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,30 @@
+import {promisify} from "node:util";
+import {pipeline} from "node:stream";
+import {createUnzip} from "node:zlib";
+import {createReadStream, createWriteStream} from "node:fs";
+import fs from "node:fs/promises";
+
+import {join} from "path";
+
+const FILE_PATH = join(import.meta.dirname, 'files', 'archive.gz');
+const SAVE_COMPRESSED = join(import.meta.dirname,'files', 'fileToCompress_unzipped.txt');
+
 const decompress = async () => {
-    // Write your code here 
+    try {
+        await fs.access(FILE_PATH, fs.constants.F_OK);
+
+        const pipe = promisify(pipeline);
+        const GZIP = createUnzip();
+
+        const STREAM_FROM = createReadStream(FILE_PATH);
+        const STREAM_TO = createWriteStream(SAVE_COMPRESSED);
+
+        await pipe(STREAM_FROM, GZIP, STREAM_TO);
+
+        process.stdout.write(`File location: ${SAVE_COMPRESSED}`);
+    } catch (e) {
+        throw new Error(`Error during decompression: ${e.message}`);
+    }
 };
 
 await decompress();
