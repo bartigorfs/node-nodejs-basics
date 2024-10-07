586062eaba9116ab4d681555170381ada9da36fc
Igor Bartashevich <86968311+bartigorfs@users.noreply.github.com>
2024-10-07 22:10:48 +0300

    feat: write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,24 @@
+import fs from "node:fs/promises";
+import {join} from "path";
+import {createWriteStream} from "node:fs";
+
+const FILE_PATH = join(import.meta.dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    try {
+        await fs.access(FILE_PATH, fs.constants.W_OK)
+
+        const writeStream = createWriteStream(FILE_PATH).setDefaultEncoding('utf-8');
+
+        process.stdin.pipe(writeStream);
+
+        writeStream.on('error', (err) => {
+            throw new Error(`Stream error: ${err.message}`);
+        });
+
+    } catch (e) {
+        throw new Error(`Error during read streaming: ${e.message}`);
+    }
 };
 
 await write();
