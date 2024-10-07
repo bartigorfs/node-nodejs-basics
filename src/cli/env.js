2ec404b519ab8b9c54a4978eeb948b0a9d52810a
Igor Bartashevich <86968311+bartigorfs@users.noreply.github.com>
2024-10-07 20:57:35 +0300

    feat: env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
+import {env} from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here 
+    const PARSED_ENV = [];
+    const PREFIX = 'RSS_';
+    let STDOUT_STR = '';
+
+    for (const key in env) {
+        if (key.includes(PREFIX)) { // or startsWith
+            PARSED_ENV.push(env[key]);
+        }
+    }
+
+    PARSED_ENV.forEach((item, index) => STDOUT_STR += `RSS_name${index}=${item}${PARSED_ENV.length === index + 1 ? '' : '; '}`);
+    console.log(STDOUT_STR);
 };
 
 parseEnv();
