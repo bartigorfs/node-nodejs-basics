d9829a2f3227b81c7d504e6e060460b98b2bfd41
Igor Bartashevich <86968311+bartigorfs@users.noreply.github.com>
2024-10-07 22:29:54 +0300

    feat: fs create, fs delete

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import * as fs from 'node:fs/promises';
+
+import {join} from "path";
+
+const FILE_PATH = join(import.meta.dirname, 'files', 'fresh.txt');
+
 const create = async () => {
-    // Write your code here 
+    const FILE_CONTENT = 'I am fresh and young';
+
+    try {
+        await fs.access(FILE_PATH, fs.constants.F_OK)
+        throw new Error('FS operation failed');
+    } catch (e) {
+        if (e.code === 'ENOENT') {
+            try {
+                await fs.appendFile(FILE_PATH, FILE_CONTENT)
+                    .then(() => console.log(`File ${FILE_PATH} created`));
+            } catch (e) {
+                throw new Error(`Create operation failed: ${e.message}`);
+            }
+        } else {
+            throw new Error(`Error while creating: ${e.message}`);
+        }
+    }
 };
 
 await create();
--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import fs from "node:fs/promises";
+
+import {join} from "path";
+
+const FILE_TO_DELETE = join(import.meta.dirname, 'files', 'fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        await fs.access(FILE_TO_DELETE, fs.constants.W_OK);
+        await fs.rm(FILE_TO_DELETE)
+            .then(() => console.log(`File ${FILE_TO_DELETE} removed`));
+    } catch (e) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await remove();
