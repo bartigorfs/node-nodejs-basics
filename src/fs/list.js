c7131abce057fa67232592211d66c9488e1eb498
Igor Bartashevich <86968311+bartigorfs@users.noreply.github.com>
2024-10-07 22:31:24 +0300

    feat: fs list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import fs from "node:fs/promises";
+
+import {join} from "path";
+
+const BASE_DIR = join(import.meta.dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+    try {
+        await fs.access(BASE_DIR, fs.constants.R_OK);
+        const FILE_LISTING = await fs.readdir(BASE_DIR);
+        console.log(FILE_LISTING);
+    } catch (e) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
