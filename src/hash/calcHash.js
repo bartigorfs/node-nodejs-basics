5cdb3f9184c975c715b17fe032011e87b1e13a7c
Igor Bartashevich <86968311+bartigorfs@users.noreply.github.com>
2024-10-07 21:20:44 +0300

    feat: calculate hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import * as path from "node:path";
+import * as fs from "node:fs";
+import * as crypto from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.join(import.meta.dirname, 'files', 'fileToCalculateHashFor.txt');
+    const hash = crypto.createHash('sha256');
+    const stream = fs.createReadStream(filePath);
+
+    stream.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+    stream.on('end', () => {
+        const result = hash.digest('hex');
+        console.log(`SHA256 hash for file ${filePath}: ${result}`);
+    });
+    stream.on('error', (error) => {
+        console.error(`Error while reading file: ${error.message}`);
+    });
 };
 
 await calculateHash();
