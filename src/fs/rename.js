53bb0e17305922a73428b2a6596216b65255a88a
Igor Bartashevich <86968311+bartigorfs@users.noreply.github.com>
2024-10-07 22:36:59 +0300

    feat: fs rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,28 @@
+import fs from "node:fs/promises";
+
+import {join} from "path";
+
+const BASE_DIR = join(import.meta.dirname, 'files');
+const FILE_TO_RENAME_FROM = join(BASE_DIR, 'wrongFilename.txt');
+const FILE_TO_RENAME_TO = join(BASE_DIR, 'properFilename.md');
+
 const rename = async () => {
-    // Write your code here 
+    try {
+        await fs.access(FILE_TO_RENAME_TO, fs.constants.R_OK)
+        throw new Error('FS operation failed');
+    } catch (e) {
+        if (e.code === 'ENOENT') {
+            try {
+                await fs.access(FILE_TO_RENAME_FROM, fs.constants.R_OK)
+                await fs.rename(FILE_TO_RENAME_FROM, FILE_TO_RENAME_TO)
+                    .then(() => console.log('File renamed'));
+            } catch (e) {
+                throw new Error('FS operation failed');
+            }
+        } else {
+            throw new Error(e);
+        }
+    }
 };
 
 await rename();
