153a0ba0720f689f9fc0f1a1ac4d3c6a20ddbcf2
Igor Bartashevich <86968311+bartigorfs@users.noreply.github.com>
2024-10-07 22:33:34 +0300

    feat: fs read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import fs from "node:fs/promises";
+
+import {join} from "path";
+
+const BASE_DIR = join(import.meta.dirname, 'files')
+const FILE_TO_READ = join(BASE_DIR, 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    try {
+        await fs.access(BASE_DIR, fs.constants.R_OK);
+        const FILE_CONTENT  = await fs.readFile(FILE_TO_READ, {encoding: 'utf-8'});
+        console.log(FILE_CONTENT);
+    } catch (e) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
